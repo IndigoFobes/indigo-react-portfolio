02ff9d2c506f82cada542d37c1f9553319bdd9b2
Indigo <indigo.crandell@gmail.com>
2022-11-23 12:34:03 -0500

    created a Header component and replaced the default react logo and such with my Header component.

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+// export this to be used in App.js
+export default function Header () {
+
+    return (
+        <div>
+            Hello! Starting my React App!
+        </div>
+    )
+}
+

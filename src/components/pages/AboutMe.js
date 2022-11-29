c735b6e58b6f5abc9876f1e0151bcf6836f674ca
Indigo <indigo.crandell@gmail.com>
2022-11-28 21:41:28 -0500

    about me styling

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 export default function AboutMe () {
     return (
-        <div className='container-lg gx-5 h-100'>
+        <div className='about-me container-lg gx-5 h-100'>
             <h1>About Me</h1>
             <p>
-                This is a bit about me!
+                My name is Indigo Fobes. I am a budding full-stack web developer from Alpharetta, Georgia. TBC...
             </p>
         </div>
     );
